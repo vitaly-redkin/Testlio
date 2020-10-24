83f53f82e210ef047c2c250a21a619619b6baa45
vitaly-redkin <v21267@gmail.com>
2020-10-24 17:31:51 +0300

    Tables script updated

--- tables.sql
+++ tables.sql
@@ -7,3 +7,15 @@ created_at TIMESTAMP NOT NULL DEFAULT now(),
 updated_by VARCHAR(100),
 updated_at TIMESTAMP NOT NULL DEFAULT now()
 );
+
+CREATE TABLE `issue_revisions` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `issue_id` int NOT NULL,
+  `revision_no` int NOT NULL,
+  `changes` json NOT NULL,
+  `updated_by` varchar(255) DEFAULT 'unknown',
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `issue_id` (`issue_id`),
+  CONSTRAINT `issue_revisions_ibfk_1` FOREIGN KEY (`issue_id`) REFERENCES `issues` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+);
