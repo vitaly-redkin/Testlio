b72b2ed20ad264c169f51922bef927bafb4525a8
vitaly-redkin <v21267@gmail.com>
2020-10-24 10:27:32 +0300

    GetList, Post and Put end points created

--- index.js
+++ index.js
@@ -6,8 +6,21 @@ const router = require('./lib/routes');
 const PORT = 8080;
 
 app.use(require('koa-bodyparser')());
+app.use(async (ctx, next) => {
+  // the parsed body will store in ctx.request.body
+  // if nothing was parsed, body will be an empty object {}
+  ctx.body = ctx.request.body;
+  await next();
+});
+
 app.use(router.routes());
 app.use(router.allowedMethods());
+
+// Syncing ORM models structure with the database
+const sequelize = require('./lib/models/connection');
+console.log("Syncing models...");
+sequelize.sync({ alter: true }).done();
+console.log("All models were synchronized successfully.");
 
 app.listen(PORT);
 console.log('Listening on http://localhost:%s/', PORT);
